cdb67c8c6e21aa428db41a64e7db908c9af51eb3
Henry Han <henryyy3@hotmail.com>
2019-10-31 11:37:53 +0800

    Code for fighting section, but with progressive errors.

--- Fighting.py
+++ Fighting.py
@@ -13,7 +13,7 @@ playerSkills = {
 
 opponentSkills = ['Smash', 'Roar', 'Punch', 'Slap']
 
-coinFlipChoice = ['Heads', 'Tails']
+coinFlipChoice = ['heads', 'tails']
 
 username = input("Enter username: ")
 print("Welcome to a fight " + username + ' .')
@@ -30,7 +30,7 @@ while fight:
 
         while coinFlip:
             print("\nChoose heads or tails to determine who starts")
-            coinChoice = input("[Heads] / [Tails]")
+            coinChoice = input("[Heads] / [Tails]").lower()
 
             if coinChoice not in coinFlipChoice:
                 print("Invalid response. Heads or Tails? \n")
@@ -45,10 +45,56 @@ while fight:
                 if coinFlip == coinChoice:
                     print("\nYou are starting.")
 
-                if coinFlip != coinChoice:
+                elif coinFlip != coinChoice:
                     print("\nGorillakong is starting.")
 
-            print(username + " is starting at " + str(playerHealth) + " healthpoints.\nGorillakong is starting at " + str(opponentHealth) + " healthpoints." )
+            print(username + " is starting at " + str(playerHealth) + " healthpoints.\nGorillakong is starting at " + str(opponentHealth) + " healthpoints.\n" )
+
+            if coinFlip != coinChoice:
+                print("Gorillakong used " + random.choice(opponentSkills) + ".")
+
+                if random.choice(opponentSkills) == 'Smash':
+                    playerHealth -= 60
+                    print(username + " is at" + str(playerHealth) + ".")
+                    print("Gorillakong is at" + str(opponentHealth) + ".")
+
+                elif random.choice(opponentSkills) == 'Roar':
+                    playerHealth -= 20
+                    print(username + " is at" + str(playerHealth) + ".")
+                    print("Gorillakong is at" + str(opponentHealth) + ".")
+
+                elif random.choice(opponentSkills) == 'Punch':
+                    playerHealth -= 40
+                    print(username + " is at" + str(playerHealth) + ".")
+                    print("Gorillakong is at" + str(opponentHealth) + ".")
+
+                elif random.choice(opponentSkills) == 'Slap':
+                    playerHealth -= 30
+                    print(username + " is at" + str(playerHealth) + ".")
+                    print("Gorillakong is at" + str(opponentHealth) + ".")
+
+            elif coinFlip == coinChoice:
+                print(username + " used " + random.choice(playerSkills) + ".")
+
+                if random.choice(playerSkills) == 'Jab':
+                    opponentHealth -= 20
+                    print(username + " is at" + str(playerHealth) + ".")
+                    print("Gorillakong is at" + str(opponentHealth) + ".")
+
+                elif random.choice(playerSkills) == 'Heavy Swing':
+                    opponentHealth -= 50
+                    print(username + " is at" + str(playerHealth) + ".")
+                    print("Gorillakong is at" + str(opponentHealth) + ".")
+
+                elif random.choice(playerSkills) == 'Roundhouse Kick':
+                    opponentHealth -= 70
+                    print(username + " is at" + str(playerHealth) + ".")
+                    print("Gorillakong is at" + str(opponentHealth) + ".")
+
+                elif random.choice(playerSkills) == 'Guard':
+                    opponentHealth += 30
+                    print(username + " is at" + str(playerHealth) + ".")
+                    print("Gorillakong is at" + str(opponentHealth) + ".")
     else:
         print("Systems shutting down..")
         break
