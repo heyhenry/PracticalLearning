0866b1f889ac538a2c6e6caaace4d9a12574eb30
Henry Han <henryyy3@hotmail.com>
2019-12-20 15:36:27 +0800

    Adding different types of widgets (Radio, ScrolledText, Messagebox).

--- Tkinter/tinks.py
+++ Tkinter/tinks.py
@@ -24,7 +24,7 @@
 #
 # window.mainloop()
 
-# # Combobox widget
+# # Combo Box widget
 # from tkinter import *
 # from tkinter.ttk import *
 #
@@ -39,20 +39,75 @@
 # combo.grid(column=1, row=0)
 # window.mainloop()
 
-# Check button widget
-from tkinter import *
-from tkinter.ttk import *
+# # Check button widget
+# from tkinter import *
+# from tkinter.ttk import *
+#
+# window = Tk()
+# window.title("Welcome to Tinks4Fun app")
+# window.geometry('450x300')
+# chk_state = BooleanVar()
+# # Can also use an alternative
+# # chk_state = IntVar()
+# # chk_state.set(0) # True
+# # chk_state.set(1) # False
+# chk_state.set(True) # set check state
+# chk = Checkbutton(window, text='Choose', var=chk_state)
+# chk.grid(column=0, row=0)
+# window.mainloop()
 
-window = Tk()
-window.title("Welcome to Tinks4Fun app")
-window.geometry('450x300')
-chk_state = BooleanVar()
-# Can also use an alternative
-# chk_state = IntVar()
-# chk_state.set(0) # True
-# chk_state.set(1) # False
-chk_state.set(True) # set check state
-chk = Checkbutton(window, text='Choose', var=chk_state)
-chk.grid(column=0, row=0)
-window.mainloop()
+# # Radio button widget
+# from tkinter import *
+# from tkinter.ttk import *
+#
+# window = Tk()
+# window.title("Welcome to Tinks4Fun")
+# window.geometry('450x300')
+# lbl = Label(window, text="Radio button:")
+# lbl.grid(column=0, row=0)
+# selected = IntVar()
+# rad1 = Radiobutton(window, text='First', value=1, variable=selected)
+# rad2 = Radiobutton(window, text='Second', value=2, variable=selected)
+# rad3 = Radiobutton(window, text='Third', value=3, variable=selected)
+#
+# def clicked():
+#     # Prints the value of the button chosen
+#     print(selected.get())
+# btn = Button(window, text="Click Me", command=clicked)
+# rad1.grid(column=1, row=0)
+# rad2.grid(column=2, row=0)
+# rad3.grid(column=3, row=0)
+# btn.grid(column=4, row=0)
+# window.mainloop()
 
+# # ScrolledText widget
+# from tkinter import *
+# from tkinter import scrolledtext
+#
+# window = Tk()
+# window.title("Welcome to Tinks4Fun app")
+# window.geometry('450x300')
+#
+# txt = scrolledtext.ScrolledText(window, width=40, height=10)
+# txt.grid(column=0, row=0)
+# txt.insert(INSERT, "Your text goes here ")
+# # To clear the contents of a ScrolledText widget
+# # txt.delete(1.0, END)
+#
+# window.mainloop()
+
+# # Message Box Widget
+# from tkinter import *
+# from tkinter import messagebox
+#
+# window = Tk()
+# window.title("Welcome to Tinks4Fun app")
+# window.geometry('450x300')
+#
+# def clicked():
+#     messagebox.showinfo("This is the message box title", "Hey, this is the content of the message box!")
+#
+# btn = Button(window, text="Click here", command=clicked)
+# btn.grid(column=0, row=0)
+#
+# window.mainloop()
